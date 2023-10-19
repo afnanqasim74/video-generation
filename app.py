1dba08f961076228d24fb7a6b5ad31ad1fe3606e
afnanqasim74 <afnanqasim74@gmail.com>
2023-10-19 18:27:53 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+
+
+
+
+
+    return render_template("index.html")
+
+
+
+
+
+if __name__== "__name__":
+    app.run(debug=False,host=  "0.0.0.0")
